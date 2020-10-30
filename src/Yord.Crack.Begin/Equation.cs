11e529968512d599035f379c0016213e4c07f2b8
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-10-31 00:50:25 +0300

    small optimisation

--- src/Yord.Crack.Begin/Equation.cs
+++ src/Yord.Crack.Begin/Equation.cs
@@ -17,15 +17,15 @@ namespace Yord.Crack.Begin
         }
 
         //целочисленные решения уравнения a^3 + b^3 = c^3 + d^3; a, b, c, d: [1,100]
-
         public List<SolutionsDto> FindSolutions()
         {
             var c3d3Results = new Dictionary<int, List<Tuple<int, int>>>();
             for (var c = 1; c <= 100; c++)
             {
+                var c3 = Math.Pow(c, 3);
                 for (var d = 1; d <= 100; d++)
                 {
-                    var c3d3Result = (int) (Math.Pow(c, 3) + Math.Pow(d, 3));
+                    var c3d3Result = (int) (c3 + Math.Pow(d, 3));
                     if (c3d3Results.TryGetValue(c3d3Result, out var currentList))
                     {
                         currentList.Add(new Tuple<int, int>(c, d));
