7f9bb0599ace98cedc3454df86fdc38cf187dee9
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-10-31 00:44:46 +0300

    use tuple

--- src/Yord.Crack.Begin/Equation.cs
+++ src/Yord.Crack.Begin/Equation.cs
@@ -14,77 +14,42 @@ namespace Yord.Crack.Begin
             public int C { get; set; }
 
             public int D { get; set; }
-
-            public bool Equals(SolutionsDto other)
-            {
-                return A == other.A && B == other.B && C == other.C && D == other.D;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                if (obj.GetType() != GetType()) return false;
-                return Equals((SolutionsDto) obj);
-            }
-
-            public override int GetHashCode()
-            {
-                return HashCode.Combine(A, B, C, D);
-            }
         }
 
         //целочисленные решения уравнения a^3 + b^3 = c^3 + d^3; a, b, c, d: [1,100]
 
-        public class C3D3Combination
-        {
-            public int C { get; set; }
-
-            public int D { get; set; }
-
-        }
-        
         public List<SolutionsDto> FindSolutions()
         {
-            var c3d3results = new Dictionary<int, List<C3D3Combination>>();
+            var c3d3Results = new Dictionary<int, List<Tuple<int, int>>>();
             for (var c = 1; c <= 100; c++)
             {
                 for (var d = 1; d <= 100; d++)
                 {
-
-                    var c3d3result = (int)(Math.Pow(c, 3) + Math.Pow(d, 3));
-                    if (c3d3results.TryGetValue(c3d3result, out var currentList))
+                    var c3d3Result = (int) (Math.Pow(c, 3) + Math.Pow(d, 3));
+                    if (c3d3Results.TryGetValue(c3d3Result, out var currentList))
                     {
-                        currentList.Add(new C3D3Combination
-                        {
-                            C = c,
-                            D = d
-                        });
+                        currentList.Add(new Tuple<int, int>(c, d));
                     }
                     else
                     {
-                        c3d3results[c3d3result] = new List<C3D3Combination> {new C3D3Combination
+                        c3d3Results[c3d3Result] = new List<Tuple<int, int>>
                         {
-                            C = c,
-                            D = d
-                        }};
+                            new Tuple<int, int>(c, d)
+                        };
                     }
                 }
             }
 
             var solutions = new List<SolutionsDto>();
-            foreach (var (_, pairList) in c3d3results)
+            foreach (var (_, pairList) in c3d3Results)
             {
-                foreach (var pair1 in pairList)
+                foreach (var (a, b) in pairList)
                 {
-                    foreach (var pair2 in pairList)
+                    foreach (var (c, d) in pairList)
                     {
                         solutions.Add(new SolutionsDto
                         {
-                            A = pair1.C,
-                            B = pair1.D,
-                            C = pair2.C,
-                            D = pair2.D
+                            A = a, B = b, C = c, D = d
                         });
                     }
                 }
