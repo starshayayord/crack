eac67f1e67502c4c2e5d936b0563ee0bca9dfdbf
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-08 22:16:51 +0300

    Task 1.1

---
+++ src/Yord.Crack.Begin/Chapter1/Task1_1.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yord.Crack.Begin.Chapter1
+{
+    public class Task1_1
+    {
+        // Реализуйте алгоритм, определяющий все ли символы в строке встречаются только один раз
+        //[PERFECT]
+        public static bool IsUniqSymbols(string source)
+        {
+            var set = new HashSet<char>();
+            foreach (var c in source)
+            {
+                if (set.Contains(c))
+                {
+                    return false;
+                }
+
+                set.Add(c);
+            }
+
+            return true;
+        }
+        
+        public static bool IsUniqSymbols2(string source)
+        {
+            return source.ToHashSet().Count == source.Length;
+        }
+        
+        // Если запрещено использовать другие структуры данных (ASCII)
+        public static bool IsUniqSymbols3(string source)
+        {
+            if (source.Length > 256)
+            {
+                return false;
+            }
+            var asciiArr = new int[256];
+            foreach (var c in source)
+            {
+                if (asciiArr[c] == 1)
+                {
+                    return false;
+                }
+
+                asciiArr[c] = 1;
+            }
+
+            return true;
+        }
+        
+        //нельзя использовать допполнительные структуры данных
+        //строка содержит только маленькие английские буквы
+        //[PERFECT2]
+        public static bool IsUniqueChars(string str)
+        {
+            if (str.Length > 256)
+            {
+                return false;
+            }
+
+            var checker = 0;
+            for (var i = 0; i < str.Length; i++)
+            {
+                var val = str[i] - 'a'; //получили смещение относительно минимального символа. Т.е. смещение для a == 0
+
+                if ((checker & (1 << val)) > 0) //если в val'ом разряде чекера уже стоит 1, значит символ уже присутствовал
+                {
+                    return false;
+                }
+                checker |= 1 << val;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ test/Yord.Crack.Begin.Tests/Chapter1/Task1_1_Tests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using Yord.Crack.Begin.Chapter1;
+
+namespace Yord.Crack.Begin.Tests.Chapter1
+{
+    [TestFixture]
+    public class Task1_1_Tests
+    {
+        [TestCase("ab", ExpectedResult = true)]
+        [TestCase("abb", ExpectedResult = false)]
+        public bool Should_CheckUniq1_WithHashMap(string s)
+        {
+            return Task1_1.IsUniqSymbols(s);
+        }
+        
+        [TestCase("ab", ExpectedResult = true)]
+        [TestCase("abb", ExpectedResult = false)]
+        public bool Should_CheckUniq2_WithHashMap(string s)
+        {
+            return Task1_1.IsUniqSymbols2(s);
+        }
+        
+        [TestCase("ab", ExpectedResult = true)]
+        [TestCase("abb", ExpectedResult = false)]
+        public bool Should_CheckUniq3_WithASCIIArray(string s)
+        {
+            return Task1_1.IsUniqSymbols3(s);
+        }
+        
+        [TestCase("ab", ExpectedResult = true)]
+        [TestCase("abb", ExpectedResult = false)]
+        public bool Should_IsUniqueChars_WithASCIIArray(string s)
+        {
+            return Task1_1.IsUniqueChars(s);
+        }
+    }
+}
