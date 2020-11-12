e9b5e94bcc054716d2b27562806914c606655717
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-12 23:56:32 +0300

    Task 1.8 additional solution

--- src/Yord.Crack.Begin/Chapter1/Task8.cs
+++ src/Yord.Crack.Begin/Chapter1/Task8.cs
@@ -5,7 +5,52 @@ namespace Yord.Crack.Begin.Chapter1
     // Если элемент матрицы MxN равен 0, то столбец и строка обнуляются
     public class Task8
     {
+        //PERFECT (чуть проще оценить сложность)
         public static int[][] ToZero(int[][] source)
+        {
+            var row = new bool[source.Length];
+            var column = new bool[source[0].Length];
+            for (var r = 0; r < source.Length; r++)
+            {
+                for (var c = 0; c < source[r].Length; c++)
+                {
+                    if (column[c])
+                    {
+                        continue;
+                    }
+                    if (source[r][c] == 0)
+                    {
+                        row[r] = true;
+                        column[c] = true;
+                        break;
+                    }
+                }
+            }
+
+            for (var r = 0; r < row.Length; r++)
+            {
+                if (row[r])
+                {
+                    source[r] = new int [source[r].Length];
+                }
+            }
+            
+            for (var c = 0; c < column.Length; c++)
+            {
+                if (column[c])
+                {
+                    for (var i = 0; i < source.Length; i++)
+                    {
+                        source[i][c] = 0;
+                    }
+                }
+            }
+
+            return source;
+        }
+        
+        //  сложнее оцнить сложность
+        public static int[][] ToZero2(int[][] source)
         {
             var zeroColumns = new HashSet<int>();
             for (var r = 0; r < source.Length; r++)
--- test/Yord.Crack.Begin.Tests/Chapter1/Task8_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter1/Task8_Tests.cs
@@ -21,5 +21,21 @@ namespace Yord.Crack.Begin.Tests.Chapter1
             CollectionAssert.AreEqual(new[] {0, 0, 0, 0}, replaced[1]);
             CollectionAssert.AreEqual(new[] {0, 0, 0, 0}, replaced[2]);
         }
+        
+        [Test]
+        public void Should_ReplaceElements2_Successfully()
+        {
+            var source = new[]
+            {
+                new[] {1, 2, 3, 4},
+                new[] {5, 6, 0, 8},
+                new[] {0, 10, 11, 12}
+            };
+            
+            var replaced = Task8.ToZero2(source);
+            CollectionAssert.AreEqual(new[] {0, 2, 0, 4}, replaced[0]);
+            CollectionAssert.AreEqual(new[] {0, 0, 0, 0}, replaced[1]);
+            CollectionAssert.AreEqual(new[] {0, 0, 0, 0}, replaced[2]);
+        }
     }
 }
