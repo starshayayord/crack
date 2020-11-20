1b4cb90eea6bfffc90b65ac3f174876934563ab4
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-21 00:19:21 +0300

    Task 3.1, additional solution

--- src/Yord.Crack.Begin/Chapter3/Task1.cs
+++ src/Yord.Crack.Begin/Chapter3/Task1.cs
@@ -1,18 +1,211 @@
 using System;
+using System.Linq;
 
 namespace Yord.Crack.Begin.Chapter3
 {
     // из 1 одномерного массива реализовать 3 стека
     public class Task1
     {
+        // PERFECT
+        // эффективное использование пространства массива
+        // массив как бы закольцован, т.е. стек может начинаться в конце массива и заканчиваться в начале
+        // поэтому есть метод получения абсолютного индекса в _values;
         public class ArrStack<T>
+        {
+            private T[] _values;
+            private StackInfo[] _stacksInfo;
+
+            public ArrStack(int defaultSize, int stacksAmount)
+            {
+                _values = new T[defaultSize * stacksAmount];
+                _stacksInfo = new StackInfo[stacksAmount];
+                for (var i = 0; i < stacksAmount; i++)
+                {
+                    _stacksInfo[i] = new StackInfo(defaultSize * i, defaultSize);
+                }
+            }
+
+            public void Push(int stackNumber, T value)
+            {
+                //если все стеки уже полные
+                if (_stacksInfo.Sum(s => s._size) == _values.Length) throw new IndexOutOfRangeException();
+
+                var stackInfo = _stacksInfo[stackNumber];
+                //расширяем стек, если он заполнен
+                if (stackInfo.IsFull())
+                {
+                    Expand(stackNumber);
+                }
+
+                stackInfo._size++;
+                _values[GetAbsIndex(stackInfo.LastElementIndex)] = value;
+            }
+
+            public T Pop(int stackNumber)
+            {
+                var stackInfo = _stacksInfo[stackNumber];
+                if (stackInfo.IsEmpty) throw new IndexOutOfRangeException();
+                var index = GetAbsIndex(stackInfo.LastElementIndex);
+                var value = _values[index];
+                _values[index] = default;
+                stackInfo._size--;
+                return value;
+            }
+
+            public T Peek(int stackNumber)
+            {
+                var stackInfo = _stacksInfo[stackNumber];
+                return _values[GetAbsIndex(stackInfo.LastElementIndex)];
+            }
+
+            public bool IsEmpty(int stackNumber)
+            {
+                return _stacksInfo[stackNumber]._size == 0;
+            }
+
+            // для хранения информации об одном стеке
+            private class StackInfo
+            {
+                public int _startIndex;
+                public int _capacity; //вместимость
+                public int _size; //текуйщий размер
+
+                public StackInfo(int startIndex, int capacity)
+                {
+                    _startIndex = startIndex;
+                    _capacity = capacity;
+                }
+
+                public bool IsFull() => _size == _capacity;
+
+                public bool IsEmpty => _size == 0;
+
+                public int LastElementIndex => _startIndex + _size - 1;
+
+                public int LastCapacityIndex => _startIndex + _capacity - 1;
+            }
+
+            //проверяем, лежит ли индекс в границах стека. Стек может продолжаться от начала массива
+            private bool IsWithinStackCapacity(int index, int stackNumber)
+            {
+                if (index < 0 || index >= _values.Length)
+                {
+                    return false;
+                }
+
+                var stackInfo = _stacksInfo[stackNumber];
+                // при циклическом переносе индекса вносим поправку
+                // то есть например всего _values.Length = 6, _startIndex = 5, а _size = 3;
+                // то есть stack занимает индексы [5],[0],[1]
+                // в этом случае index[1] < stackInfo._startIndex[5] 
+                // и необходимо сделать его поправку на длину массива, т.е. conIndex = [6] + [1] = [7]
+                // если же брать индекс [5], то его conIndex так и останется [5]
+                var conIndex = index < stackInfo._startIndex ? index + _values.Length : index;
+                var end = stackInfo._startIndex + stackInfo._capacity;
+                return stackInfo._startIndex <= conIndex && conIndex < end;
+            }
+
+            private void Expand(int stackNumber)
+            {
+                Shift((stackNumber + 1) % _stacksInfo.Length);
+                _stacksInfo[stackNumber]._capacity++;
+            }
+
+            private void Shift(int stackNumber)
+            {
+                var stackInfo = _stacksInfo[stackNumber];
+                //если этот стек заполнен, то двигаем следующий стек на 1 элемент
+                // (если в следующем стеке места нет, то двигаем следующий и т. д.),
+                // на них будем:
+                // перемещать элементы на 1 ближе к концу, чистить бывший начальный элемент и урезать вместимость
+                // (тем самым освободим бывший начальный элемент под конец стека предыдущего)
+                if (stackInfo._size > stackInfo._capacity)
+                {
+                    var nextStackNumber = (stackNumber + 1) % _stacksInfo.Length;
+                    Shift(nextStackNumber);
+                    stackInfo._capacity++;
+                }
+
+                //берем индекс потенциально последнего элемента в стеке
+                var index = GetAbsIndex(stackInfo.LastCapacityIndex);
+                //и все элементы двигаем на 1 в сторону конца, освободив один индекс впереди
+                while (IsWithinStackCapacity(index, stackNumber))
+                {
+                    var prevIndex = GetAbsIndex(index - 1);
+                    _values[index] = _values[prevIndex];
+                    index = prevIndex;
+                }
+
+                _values[stackInfo._startIndex] = default;
+                stackInfo._startIndex = GetAbsIndex(index + 1);
+                stackInfo._capacity--; //урезаем вместимость 
+            }
+
+            //получить индекс в диапазоне 0 -> _values.Length -1
+            private int GetAbsIndex(int index)
+            {
+                var max = _values.Length;
+                return (index % max + max) % max;
+            }
+        }
+
+        //элементы из разных стеков располагаются: 1,1,1...,2,2,2...,3,3,3...
+        //реализация приятнее, чем ArrStack1
+        //если один стек намного длиннее других, то распределение свободного места неоптимально 
+        public class ArrStack2<T>
+        {
+            private const int StackSize = 10;
+            private readonly T[] _baseArray;
+            private int[] _stackPointers = {-1, -1, -1};
+
+            public ArrStack2()
+            {
+                _baseArray = new T[StackSize * 3];
+            }
+
+            public void Push(T value, int stackNumber)
+            {
+                if (_stackPointers[stackNumber] + 1 > StackSize) throw new IndexOutOfRangeException();
+                _baseArray[GetIndex(stackNumber) + 1] = value;
+                _stackPointers[stackNumber]++;
+            }
+
+            public T Pop(int stackNumber)
+            {
+                if (_stackPointers[stackNumber] == -1) throw new IndexOutOfRangeException();
+                var value = _baseArray[GetIndex(stackNumber)];
+                _baseArray[GetIndex(stackNumber)] = default;
+                _stackPointers[stackNumber]--;
+                return value;
+            }
+
+            public bool IsEmpty(int stackNumber)
+            {
+                return _stackPointers[stackNumber] == -1;
+            }
+
+            public T Peek(int stackNumber)
+            {
+                if (IsEmpty(stackNumber)) throw new IndexOutOfRangeException();
+                return _baseArray[GetIndex(stackNumber)];
+            }
+
+            private int GetIndex(int stackNumber)
+            {
+                return stackNumber * StackSize + _stackPointers[stackNumber];
+            }
+        }
+
+        //элементы из разных стеков располагаются: 1,2,3,1,2,3,1,2,3...
+        //если один стек намного длиннее других, то распределение свободного места неоптимально   
+        public class ArrStack1<T>
         {
             private readonly T[] _baseArray;
             private int? _firstIndex;
             private int? _secondIndex;
             private int? _thirdIndex;
 
-            public ArrStack()
+            public ArrStack1()
             {
                 _baseArray = new T[30];
             }
--- test/Yord.Crack.Begin.Tests/Chapter3/Task1_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter3/Task1_Tests.cs
@@ -9,7 +9,63 @@ namespace Yord.Crack.Begin.Tests.Chapter3
         [Test]
         public void Should_PushAndPop_AllLists()
         {
-            var stacks = new Task1.ArrStack<int>();
+            var stacks = new Task1.ArrStack<int>(3, 3);
+            stacks.Push(0, 1);
+            stacks.Push(0, 10);
+            stacks.Push(1, 2);
+            stacks.Push(1, 20);
+            stacks.Push(2, 3);
+            stacks.Push(2, 30);
+            
+            Assert.IsFalse(stacks.IsEmpty(0));
+            Assert.IsFalse(stacks.IsEmpty(1));
+            Assert.IsFalse(stacks.IsEmpty(2));
+            Assert.AreEqual(10, stacks.Peek(0));
+            Assert.AreEqual(10, stacks.Pop(0));
+            Assert.AreEqual(1, stacks.Pop(0));
+            Assert.AreEqual(20, stacks.Peek(1));
+            Assert.AreEqual(20, stacks.Pop(1));
+            Assert.AreEqual(2, stacks.Pop(1));
+            Assert.AreEqual(30, stacks.Peek(2));
+            Assert.AreEqual(30, stacks.Pop(2));
+            Assert.AreEqual(3, stacks.Pop(2));
+            Assert.IsTrue(stacks.IsEmpty(0));
+            Assert.IsTrue(stacks.IsEmpty(1));
+            Assert.IsTrue(stacks.IsEmpty(2));
+        }
+        
+        [Test]
+        public void Should_PushAndPop2_AllLists()
+        {
+            var stacks = new Task1.ArrStack2<int>();
+            stacks.Push(1, 0);
+            stacks.Push(10, 0);
+            stacks.Push(2, 1);
+            stacks.Push(20, 1);
+            stacks.Push(3, 2);
+            stacks.Push(30, 2);
+            
+            Assert.IsFalse(stacks.IsEmpty(0));
+            Assert.IsFalse(stacks.IsEmpty(1));
+            Assert.IsFalse(stacks.IsEmpty(2));
+            Assert.AreEqual(10, stacks.Peek(0));
+            Assert.AreEqual(10, stacks.Pop(0));
+            Assert.AreEqual(1, stacks.Pop(0));
+            Assert.AreEqual(20, stacks.Peek(1));
+            Assert.AreEqual(20, stacks.Pop(1));
+            Assert.AreEqual(2, stacks.Pop(1));
+            Assert.AreEqual(30, stacks.Peek(2));
+            Assert.AreEqual(30, stacks.Pop(2));
+            Assert.AreEqual(3, stacks.Pop(2));
+            Assert.IsTrue(stacks.IsEmpty(0));
+            Assert.IsTrue(stacks.IsEmpty(1));
+            Assert.IsTrue(stacks.IsEmpty(2));
+        }
+        
+        [Test]
+        public void Should_PushAndPop1_AllLists()
+        {
+            var stacks = new Task1.ArrStack1<int>();
             stacks.Push(1, 1);
             stacks.Push(10, 1);
             stacks.Push(2, 2);
