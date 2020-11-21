29a075f5df1dda6f735ca5269e9ea798d5ba2273
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-21 23:28:30 +0300

    add task 3.3, additional

--- src/Yord.Crack.Begin/Chapter3/Task3.cs
+++ src/Yord.Crack.Begin/Chapter3/Task3.cs
@@ -9,14 +9,172 @@ namespace Yord.Crack.Begin.Chapter3
     // Дополнительно реализовать функцию PopAt(int stackNumber)
     public class Task3
     {
+        // PERFECT
+        // код приятнее, чем мой. + оптимизация за счет двусвязности и знания последней ноды в стеке
         public class SetOfStacks<T>
+        {
+            private List<InnerStack> _stacks;
+
+            private readonly int MaxSize;
+
+            public SetOfStacks(int maxSize)
+            {
+                MaxSize = maxSize;
+                _stacks = new List<InnerStack>();
+            }
+
+            //пушим в последний стек. если он заполнен то создаем новый, пушим в него и кладем в список стеков
+            public void Push(T value)
+            {
+                var lastStack = _stacks.LastOrDefault();
+                if (lastStack != null && !lastStack.IsFull)
+                {
+                    lastStack.Push(value);
+                }
+                else
+                {
+                    var newStack = new InnerStack(MaxSize);
+                    newStack.Push(value);
+                    _stacks.Add(newStack);
+                }
+            }
+
+            // достаем из последнего стека
+            //если он опустел, то удаляем его из списка стеков
+            public T Pop()
+            {
+                var lastStack = _stacks.LastOrDefault();
+                if (lastStack == null) throw new IndexOutOfRangeException();
+                var value = lastStack.Pop();
+                if (lastStack.IsEmpty)
+                {
+                    _stacks.RemoveAt(_stacks.Count - 1);
+                }
+
+                return value;
+            }
+
+            public T PopAt(int index)
+            {
+                return LeftShift(index, true);
+            }
+
+            public T PeekAt(int stackNumber)
+            {
+                return _stacks[stackNumber]._top._value;
+            }
+
+            public int NumberOfStacks => _stacks.Count;
+
+
+            // возвращает удаленную ноду
+            // если это самый верхний LeftShift(), то это Pop() нужно удалить верхнюю ноду, которую извлекли
+            private T LeftShift(int index, bool removeTop = false)
+            {
+                var stack = _stacks[index];
+                // если это не верхний LeftShift, то это не Pop, и нужно удалить низ текущего стека
+                // потому что ниже в него Push() вытащенный из следующего стека низ
+                var removedValue = removeTop ? stack.Pop() : stack.RemoveBottom();
+                if (stack.IsEmpty)
+                {
+                    _stacks.RemoveAt(index);
+                }
+                else
+                {
+                    //если это не последний стек, то необходимо двигать дальше
+                    if (_stacks.Count > index + 1)
+                    {
+                        var v = LeftShift(index + 1);
+                        stack.Push(v);
+                    }
+                }
+
+                return removedValue;
+            }
+
+            private class InnerStack
+            {
+                private int _maxSize;
+                public StackNode _top;
+                public StackNode _bottom;
+                public int _size;
+
+                public InnerStack(int maxSize)
+                {
+                    _maxSize = maxSize;
+                }
+
+                public bool IsFull => _size == _maxSize;
+
+                public bool IsEmpty => _size == 0;
+
+                public void Push(T value)
+                {
+                    _size++;
+                    var newNode = new StackNode(value);
+                    if (_size == 1)
+                    {
+                        _bottom = newNode;
+                    }
+                    else
+                    {
+                        newNode._next = _top;
+                        _top._previous = newNode;
+                    }
+
+                    _top = newNode;
+                }
+
+                public T Pop()
+                {
+                    var value = _top._value;
+                    _top = _top._next;
+                    if (_top != null)
+                    {
+                        _top._previous = null;
+                    }
+                    _size--;
+                    return value;
+                }
+
+                //удаляет нижнюю ноду стека и возвращает удаленное значение
+                public T RemoveBottom()
+                {
+                    var removedItem = _bottom._value;
+                    _bottom = _bottom._previous;
+                    if (_bottom != null)
+                    {
+                        // у последней ноды не должно быть следующей
+                        _bottom._next = null;
+                    }
+
+                    _size--;
+                    return removedItem;
+                }
+            }
+
+            private class StackNode
+            {
+                public StackNode _next;
+                public StackNode _previous;
+                public T _value;
+
+                public StackNode(T value, StackNode next = null)
+                {
+                    _value = value;
+                    _next = next;
+                }
+            }
+        }
+
+        public class SetOfStacks2<T>
         {
             private readonly int MaxSize;
             private List<InnerStack> _stacks;
 
             public int NumberOfStacks => _stacks.Count;
 
-            public SetOfStacks(int maxSize)
+            public SetOfStacks2(int maxSize)
             {
                 MaxSize = maxSize;
                 _stacks = new List<InnerStack>();
--- test/Yord.Crack.Begin.Tests/Chapter3/Task3_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter3/Task3_Tests.cs
@@ -7,9 +7,31 @@ namespace Yord.Crack.Begin.Tests.Chapter3
     public class Task3_Tests
     {
         [Test]
-        public void Should_PopAndPush1_Successfully()
+        public void Should_PopAndPush_Successfully()
         {
             var set = new Task3.SetOfStacks<int>(2);
+            foreach (var i in new []{1,2,  3,4,  5,6,  7,8,  9})
+            {
+                set.Push(i);
+            }
+            
+            Assert.AreEqual(5, set.NumberOfStacks);
+            Assert.AreEqual(9, set.Pop());
+            Assert.AreEqual(4, set.NumberOfStacks);
+            Assert.AreEqual(8, set.Pop());
+            Assert.AreEqual(4, set.NumberOfStacks);
+            Assert.AreEqual(7, set.PeekAt(3));
+            Assert.AreEqual(4, set.PopAt(1));
+            Assert.AreEqual(3, set.NumberOfStacks);
+            Assert.AreEqual(2, set.PeekAt(0));
+            Assert.AreEqual(5, set.PeekAt(1));
+            Assert.AreEqual(7, set.PeekAt(2));
+        }
+        
+        [Test]
+        public void Should_PopAndPush2_Successfully()
+        {
+            var set = new Task3.SetOfStacks2<int>(2);
             foreach (var i in new []{1,2,  3,4,  5,6,  7,8,  9})
             {
                 set.Push(i);
