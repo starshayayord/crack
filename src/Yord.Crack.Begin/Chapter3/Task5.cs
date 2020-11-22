3e3502aba58220cfbb31915999284f660be8f904
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-22 23:49:50 +0300

    add Task 3.5

--- src/Yord.Crack.Begin/Chapter3/Task5.cs
+++ src/Yord.Crack.Begin/Chapter3/Task5.cs
@@ -1,6 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Yord.Crack.Begin.Chapter3
 {
+    // реализовать метод сортировки стека (псамый большой Pop() первым)
+    // можно использовать только один дополнительный стек, перекладывать зачения в другие структуры данных нельзя
     public class Task5
     {
+        public static Stack<int> Sort(Stack<int> stack)
+        {
+            var sorted = new Stack<int>();
+            while (stack.Any())
+            {
+                // берем значение из несортированного стека, пока есть, что брать
+                var tmp = stack.Pop();
+                // если оно больше или равно верхушке сортированного, то кладем его в сортированный стек 
+                // если оно меньше, то возвращаем из сортированного стека в несортированный
+                // пока это значение не станет больше или равно верхушке
+                while (sorted.Any() && tmp < sorted.Peek())
+                {
+                    stack.Push(sorted.Pop());
+                }
+                sorted.Push(tmp);
+            }
+
+            return sorted;
+        }
     }
 }
--- test/Yord.Crack.Begin.Tests/Chapter3/Task5_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter3/Task5_Tests.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Yord.Crack.Begin.Chapter3;
+
 namespace Yord.Crack.Begin.Tests.Chapter3
 {
+    [TestFixture]
     public class Task5_Tests
     {
+
+        [Test]
+        public void Should_SortStack_Successfully()
+        {
+            var array = new[] {12, 1, 8, 5, 8};
+            
+            var sortedStack = Task5.Sort(Initialize(array));
+
+            foreach (var a in array.OrderByDescending(i => i))
+            {
+                Assert.AreEqual(a, sortedStack.Pop());
+            }
+        }
         
+        private Stack<int> Initialize(int[] array)
+        {
+            var stack = new Stack<int>();
+            foreach (var i in array)
+            {
+                stack.Push(i);
+            }
+
+            return stack;
+        }
     }
 }
