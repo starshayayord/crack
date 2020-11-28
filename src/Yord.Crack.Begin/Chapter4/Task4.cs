ce6bbc4db13b6983813fa9e5002bab2ea28941b3
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-11-28 22:38:26 +0300

    add task 4.4 commants

--- src/Yord.Crack.Begin/Chapter4/Task4.cs
+++ src/Yord.Crack.Begin/Chapter4/Task4.cs
@@ -7,8 +7,8 @@ namespace Yord.Crack.Begin.Chapter4
     // проверить сбалансированность бинарного дерева, т.е. разница высот двух поддеревьев любого узла <=1
     public class Task4
     {
-        // PERFECT
-        // рекурсивно получаем высоту левого и правого узла для каждого поддерева, пока нет ошибки
+        // PERFECT O(N)
+        // рекурсивно получаем высоту левого и правого узла для каждого поддерева, пока нет ошибки (-1)
         public static bool IsBalanced(BTNode root)
         {
             return CheckHeight(root) != -1;
@@ -17,18 +17,22 @@ namespace Yord.Crack.Begin.Chapter4
         // спускаемся до низа левого поддерева без подсчета,  когда дошли до первой null ноды, вернем высоту 0 левого
         // идем проверять высоту правого поддерева
         // при каждом приходе в null ноду (лист) возвращаем 0
-        // при кадом откате назад берем высоту максимального поддерева текущего узла и +1, потому что идем на уровень выше
+        // при каждом откате назад берем высоту максимального поддерева текущего узла и +1,
+        // потому что идем на уровень выше, надо учесть высоту текущей ноды
         // возвращенную высоту левого сравниваем с рекурсивно возвращенной высотой правого
         // если разница уже дала несбалансированное дерево, то разматываем рекурсию назад с ошибочным кодом "-1"
         private static int CheckHeight(BTNode n)
         {
             if (n == null) return 0;
+
             var leftHeight = CheckHeight(n.Left);
-            if (leftHeight == -1) return -1;
+            if (leftHeight == -1) return -1; //прокидывает код ошибки дальше
+
             var rightHeight = CheckHeight(n.Right);
-            if (rightHeight == -1) return -1;
-            if (Math.Abs(leftHeight - rightHeight) > 1) return -1;
-            return Math.Max(leftHeight, rightHeight) + 1;
+            if (rightHeight == -1) return -1; //прокидывает код ошибки дальше
+
+            if (Math.Abs(leftHeight - rightHeight) > 1) return -1; //несбалансированное, код ошибки
+            return Math.Max(leftHeight, rightHeight) + 1; // возвращаем высоту поддерева
         }
 
 
