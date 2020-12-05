d528f3f4df47ed063553fb801b9f773ad6473314
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-12-06 00:44:33 +0300

    add task 4.8

--- src/Yord.Crack.Begin/Chapter4/Task8.cs
+++ src/Yord.Crack.Begin/Chapter4/Task8.cs
@@ -3,11 +3,54 @@ namespace Yord.Crack.Begin.Chapter4
     // найти первого общего предка двух узлов бинарного дерева, не использовать дополнительные структуры данных
     public class Task8
     {
-        public static BTNode GetAncestor(BTNode n1, BTNode n2)
+        public static BTNode GetAncestor(BTNode root, BTNode n1, BTNode n2)
         {
-            
+            var r = FirstAncestor(root, n1, n2);
+            return r.IsAncestor ? r.Node : null;
         }
-        
+
+
+        private static Ancestor FirstAncestor(BTNode root, BTNode n1, BTNode n2)
+        {
+            if (root == null) return new Ancestor(false); // дошли до низа безуспешно
+            // нода равна сразу обоим узлам, возвращаем этот узел
+            if (root == n1 && root == n2) return new Ancestor(true, root); 
+            // ищем предка слева
+            var rl = FirstAncestor(root.Left, n1, n2);
+            if (rl.IsAncestor) return rl;
+            // ищем предка справа
+            var rr = FirstAncestor(root.Right, n1, n2);
+            if (rr.IsAncestor) return rr;
+            // в каждом из поддеревьев есть по одной ноде, значит текущий - искомый предок
+            if (rl.Node != null && rr.Node != null)
+            {
+                return new Ancestor(true, root);
+            }
+            // текущий узел равен одному из тех, у которых ищем родителя
+            // значит мы нашли как минимум родителя одного из узлов
+            // или обоих, если нашли по ноде в поддереве
+            if (root == n1 || root == n2)
+            {
+                return new Ancestor(rl.Node != null || rr.Node != null, root);
+            }
+
+            // родителя не нашли, ненулевую ноду запоминаем
+            return new Ancestor(false, rl.Node ?? rr.Node);
+        }
+
+
+        private class Ancestor
+        {
+            public BTNode Node;
+            public bool IsAncestor;
+
+            public Ancestor(bool isAncestor, BTNode node = null)
+            {
+                Node = node;
+                IsAncestor = isAncestor;
+            }
+        }
+
         public class BTNode
         {
             public int Value;
--- test/Yord.Crack.Begin.Tests/Chapter4/Task8_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter4/Task8_Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Yord.Crack.Begin.Chapter4;
 
 namespace Yord.Crack.Begin.Tests.Chapter4
 {
@@ -6,8 +7,86 @@ namespace Yord.Crack.Begin.Tests.Chapter4
     public class Task8_Tests
     {
         [Test]
-        public void Should()
-        {}
+        public void Should_FindAncestor_SameSide()
+        {
+            var tree = GetTreeSameSide();
+
+            var ancestor = Task8.GetAncestor(tree.Root, tree.N1, tree.N2);
+            
+            Assert.AreEqual(tree.Ancestor, ancestor);
+        }
         
+        [Test]
+        public void Should_FindAncestor_Successfully()
+        {
+            var tree = GetTree();
+
+            var ancestor = Task8.GetAncestor( tree.Root, tree.N1, tree.N2);
+            
+            Assert.AreEqual(tree.Ancestor, ancestor);
+        }
+        
+        private AncestorResult GetTreeSameSide()
+        {
+            var n2 = new Task8.BTNode
+            {
+                Value = 3
+            };
+            var n1 = new Task8.BTNode
+            {
+                Value = 2,
+                Left = n2
+            };
+           
+            var root = new Task8.BTNode
+            {
+                Value = 1,
+                Left = n1
+            };
+            return new AncestorResult
+            {
+                Root = root,
+                Ancestor = n1,
+                N1 = n1,
+                N2 = n2
+            };
+        }
+
+        private AncestorResult GetTree()
+        {
+            var n1 = new Task8.BTNode
+            {
+                Value = 2
+            };
+            var n2 = new Task8.BTNode
+            {
+                Value = 4
+            };
+            var root = new Task8.BTNode
+            {
+                Value = 1,
+                Left = n1,
+                Right = new Task8.BTNode
+                {
+                    Value = 3,
+                    Right = n2
+                }
+            };
+            return new AncestorResult
+            {
+                Root = root,
+                Ancestor = root,
+                N1 = n1,
+                N2 = n2
+            };
+        }
+
+        private class AncestorResult
+        {
+            public Task8.BTNode Root;
+            public Task8.BTNode N1;
+            public Task8.BTNode N2;
+            public Task8.BTNode Ancestor;
+        }
     }
 }
