056bedb996fd9713a3297c5b0c37340209205755
Olga Linikova <olga.linikova@alibaba-inc.com>
2020-12-22 23:23:47 +0300

    add task 5.1 stupid

---
+++ src/Yord.Crack.Begin/Chapter5/Task1.cs
@@ -0,0 +1,32 @@
+namespace Yord.Crack.Begin.Chapter5
+{
+    //для 32 разрядных M и N написать вставку M в N начиная с i и до j позиции (помещается гарантированно)
+    //N = 10000000000 M=10011, i=2, j=6 => 10001001100
+    public class Task1
+    {
+        public static int Insert(int n, int m, int i, int j)
+        {
+            var mLen = j - i;
+            for (var c = 0; c <= mLen; c++)
+            {
+                var value = GetBit(m, c);
+                n = SetBit(n, i + c, value);
+            }
+
+            return n;
+        }
+
+        private static int SetBit(int n, int i, int value)
+        {
+            var mask = ~(1 << i);
+            return n & mask | (value << i);
+        }
+
+        private static int GetBit(int m, int i)
+        {
+            var mask = 1 << i;
+            var isOne = (m & mask) != 0;
+            return isOne ? 1 : 0;
+        }
+    }
+}
---
+++ test/Yord.Crack.Begin.Tests/Chapter5/Task1_Tests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Yord.Crack.Begin.Chapter5;
+
+namespace Yord.Crack.Begin.Tests.Chapter5
+{
+    [TestFixture]
+    public class Task1_Tests
+    {
+        [Test]
+        public void Should_InsertMIntoN_Successfully()
+        {
+            Assert.AreEqual(1100, Task1.Insert(1024, 19,2,6));
+        }
+    }
+}
