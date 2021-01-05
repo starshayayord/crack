439832ee8972c59f79a4514172c228f0d60659b5
Olga Linikova <olga.linikova@alibaba-inc.com>
2021-01-06 01:35:20 +0300

    add task 5.4

---
+++ test/Yord.Crack.Begin.Tests/Chapter5/Task4_Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using Yord.Crack.Begin.Chapter5;
+
+namespace Yord.Crack.Begin.Tests.Chapter5
+{
+    [TestFixture]
+    public class Task4_Tests
+    {
+        [Test]
+        public void Should_GetClosestMinMaxSamiBits()
+        {
+            var r = Task4.GetMaxMinSameBits(13948);
+            
+            Assert.AreEqual(13946, r.Min);
+            Assert.AreEqual(13967, r.Max);
+        }
+    }
+}
