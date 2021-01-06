430c0c0cc139fa601733976224b4d9a104c51275
Olga Linikova <olga.linikova@alibaba-inc.com>
2021-01-06 22:09:53 +0300

    add task 5.4 another solution

--- src/Yord.Crack.Begin/Chapter5/Task4.cs
+++ src/Yord.Crack.Begin/Chapter5/Task4.cs
@@ -13,6 +13,84 @@ namespace Yord.Crack.Begin.Chapter5
             public int Max { get; set; }
         }
 
+        public static Result GetMaxMinSameBitsMath(int n)
+        {
+            return new Result
+            {
+                Min = GetMinMath(n),
+                Max = GetMaxMath(n)
+            };
+        }
+
+        private static int GetMaxMath(int n)
+        {
+            var c = n;
+            var c0 = 0; // кол-во завершающих нулей
+            var c1 = 0; // размер последующего блока единиц
+            while ((c & 1) == 0 && c != 0)
+            {
+                c0++;
+                c >>= 1;
+            }
+
+            while ((c & 1) == 1)
+            {
+                c1++;
+                c >>= 1;
+            }
+
+            if (c0 + c1 == 0 || c0 + c1 == 31) return -1;
+            var p = c0 + c1;
+            //необходимо:
+            //установить Рый бит
+            //сбросить все биты после Р
+            //установить биты с 0 по (с1-2), т.е. всего с1-1 битов
+            n += (int) Math.Pow(2, c0) - 1; //установим завершающие нули в 0, получив Р завершающих единиц
+            n += 1; //сбросим первые Р единиц и установим бит в Рой позиции
+            n += (int) Math.Pow(2, c1 - 1) - 1; //установим завершающие (c1-1) нулей в единицы
+            return n;
+            //по сути решение сводится к:
+            // = n+(int)Math.Pow(2, c0) - 1 + 1 + (int) Math.Pow(2, c1 - 1) - 1 = 
+            // n + (int)Math.Pow(2, c0) + (int) Math.Pow(2, c1 - 1) -1
+            //что можно записать как 
+            // = n + (1 << c0) + (1 << (c1 -1)) -1;
+        }
+
+        private static int GetMinMath(int n)
+        {
+            var c = n;
+            var c0 = 0; // размер блока соседних с ними нулей
+            var c1 = 0; // кол-во конечных единиц
+            while ((c & 1) == 1)
+            {
+                c1++;
+                c >>= 1;
+            }
+
+            if (c == 0) return -1;
+
+            while ((c & 1) == 0 && c != 0)
+            {
+                c0++;
+                c >>= 1;
+            }
+
+            var p = c0 + c1;
+            // необходимо:
+            // сбросить Рый бит
+            // установить все биты после Р
+            // сбросить биты с 0 по (c0-1)
+            n -= (int) Math.Pow(2, c1) - 1; //удалили завершающие единицы
+            n -= 1;//установили все биты после Р (переключили завершающие нули)
+            n -= (int) Math.Pow(2, c0 - 1) - 1; //сброс ПОСЛЕДУЮЩИХ (с0-1) нулей 
+            return n;
+            //по сути решение сводится к:
+            // = n - (int) Math.Pow(2, c1) - 1  +1 -( (int) Math.Pow(2, c0 - 1) - 1)
+            // = n - Math.Pow(2, c1)  - Math.Pow(2, c0 - 1) +1
+            //что можно записать как 
+            // = n - (1 << c1) - (1 << (c0 -1)) +1;
+        }
+
         public static Result GetMaxMinSameBits(int n)
         {
             return new Result
@@ -21,7 +99,7 @@ namespace Yord.Crack.Begin.Chapter5
                 Max = GetMax(n)
             };
         }
-        
+
         private static int GetMax(int n)
         {
             //допустим Р - позиция самого правого незавершающего нуля
@@ -29,13 +107,14 @@ namespace Yord.Crack.Begin.Chapter5
             var c0 = 0;
             var c1 = 0;
             // двигаем до первой единицы, т.е. с0 - кол-во нулей справа от Р
-            while ((c&1) == 0 && c!=0)
+            while ((c & 1) == 0 && c != 0)
             {
                 c0++;
                 c >>= 1;
             }
+
             //двигаем дальше до первого нуля, т.е. c1 - кол-во единиц справа от Р
-            while ((c&1) == 1)
+            while ((c & 1) == 1)
             {
                 c1++;
                 c >>= 1;
@@ -51,11 +130,11 @@ namespace Yord.Crack.Begin.Chapter5
             var p = c0 + c1;
 
             n |= 1 << p; // установили Рый бит в единицу
-            n &= ~((1 << p) - 1);//сбросили все биты правее Р
+            n &= ~((1 << p) - 1); //сбросили все биты правее Р
             n |= (1 << (c1 - 1)) - 1; //вставили с1-1 единиц справа от Р
             return n;
         }
-        
+
 
         private static int GetMin(int n)
         {
@@ -64,7 +143,7 @@ namespace Yord.Crack.Begin.Chapter5
             var c0 = 0;
             var c1 = 0;
             // двигаем до первого нуля, т.е. с1 - кол-во единиц справа от Р
-            while ((c&1) == 1)
+            while ((c & 1) == 1)
             {
                 c1++;
                 c >>= 1;
@@ -75,9 +154,9 @@ namespace Yord.Crack.Begin.Chapter5
             {
                 return -1;
             }
-            
+
             //двигаем до первой единицы, т.е. с0 - кол-во нулей справа от Р
-            while ((c&1) == 0 && c !=0)
+            while ((c & 1) == 0 && c != 0)
             {
                 c0++;
                 c >>= 1;
--- test/Yord.Crack.Begin.Tests/Chapter5/Task4_Tests.cs
+++ test/Yord.Crack.Begin.Tests/Chapter5/Task4_Tests.cs
@@ -7,6 +7,15 @@ namespace Yord.Crack.Begin.Tests.Chapter5
     public class Task4_Tests
     {
         [Test]
+        public void Should_GetClosestMinMaxSamiBits_Math()
+        {
+            var r = Task4.GetMaxMinSameBitsMath(13948);
+            
+            Assert.AreEqual(13946, r.Min);
+            Assert.AreEqual(13967, r.Max);
+        }
+        
+        [Test]
         public void Should_GetClosestMinMaxSamiBits()
         {
             var r = Task4.GetMaxMinSameBits(13948);
