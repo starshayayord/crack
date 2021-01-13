1bfa46c037fa128318b1c8a12bd4a400ca445974
Olga Linikova <olga.linikova@alibaba-inc.com>
2021-01-14 00:02:47 +0300

    add task 6.2

---
+++ test/Yord.Crack.Begin.Tests/Chapter6/Task2_Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using Yord.Crack.Begin.Chapter6;
+
+namespace Yord.Crack.Begin.Tests.Chapter6
+{
+    [TestFixture]
+    public class Task2_Tests
+    {
+        [Test]
+        public void Should()
+        {
+        }
+    }
+}
