3d63bad32017aab0b01f34f2acf7daf3b58639ce
Olga Linikova <olga.linikova@alibaba-inc.com>
2021-02-11 23:04:13 +0300

    add fib

---
+++ test/Yord.Crack.Begin.Tests/Chapter8/Task1_Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Yord.Crack.Begin.Tests.Chapter8
+{
+    [TestFixture]
+    public class Task1_Tests
+    {
+        [Test]
+        public void Should()
+        {
+            
+        }
+    }
+}
