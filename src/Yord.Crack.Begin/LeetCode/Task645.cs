e0619d2d442ef0f5f051386045c218e8de9705af
Olga Linikova <olga.linikova@alibaba-inc.com>
2021-04-14 01:21:20 +0300

    add task 654 no space

--- src/Yord.Crack.Begin/LeetCode/Task645.cs
+++ src/Yord.Crack.Begin/LeetCode/Task645.cs
@@ -1,9 +1,40 @@
+using System;
+
 namespace Yord.Crack.Begin.LeetCode
 {
     // Масси должен содержать номера от 1 до N. Но одно число задудлировалось. 
     // Вернуть повторяющееся число и верное число
     public class Task645
     {
+        public static int[] FindErrorNums_NoSpace(int[] nums)
+        {
+            int dup = -1;
+            int missing = 1;
+            foreach (var n in nums)
+            {
+                if (nums[Math.Abs(n) - 1] < 0)
+                {
+                    dup = Math.Abs(n);
+                }
+                else
+                {
+                    // инвертируем n'ый элемент. Если инвертировали дважды, значит дубликат
+                    nums[Math.Abs(n) - 1] *= -1;
+                }
+            }
+
+            //теперь все элементы в nums - отрицательные, кроме того, которого нет
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //если элемент положительный, значит по этому индексу не инвертировали
+                if (nums[i] > 0)
+                {
+                    missing = i + 1;
+                }
+            }
+
+            return new[] {dup, missing};
+        }
         public static int[] FindErrorNums(int[] nums)
         {
             int repeated = 0;
